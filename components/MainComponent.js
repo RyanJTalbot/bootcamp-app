ff77c80b67592b5158478e767ab6ff7f7504b3cf
jay51 <nenojaafar3@gmail.com>
2020-02-19 17:08:10 -0600

    add favorites component

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,7 @@ import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import CampsiteInfo from "./CampsiteInfo";
 import Reservation from "./ReservationComponent";
+import Favorites from "./FavoritesComponent";
 
 import { connect } from "react-redux";
 import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners} from "../redux/ActionCreators";
@@ -26,6 +27,29 @@ const mapDispatchToProps = {
     fetchPartners
 }
 
+
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        navigationOptions: ({navigation}) => ({ 
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 
 
 const DirectoryNavigator = createStackNavigator(
@@ -172,6 +196,20 @@ const MainNavigator = createDrawerNavigator(
                 drawerIcon: ({tintColor}) => (
                     <Icon
                         name='home'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+         Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+            drawerLabel: 'My Favorites',
+            drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='heart'
                         type='font-awesome'
                         size={24}
                         color={tintColor}
