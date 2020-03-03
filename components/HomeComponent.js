a98aef8cf1b17e5065b16f383970fa202285e83c
jay51 <neneojaafar3@gmail.com>
2020-03-03 10:39:40 -0600

    week-4 add register page and camera

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,99 +1,93 @@
-import React, { Component } from "react";
-import { View, Text, ScrollView, Animated } from "react-native";
-import { Card } from "react-native-elements";
+import React, { Component } from 'react';
+import { View, Text, Animated } from 'react-native';
+import { Card } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
-import Loading from "./LoadingComponent";
 
-import { baseUrl } from "./../shared/baseUrl";
-import { connect } from "react-redux";
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        promotions: state.promotions,
+        partners: state.partners
+    };
+}
 
-function RenderItem({item, isLoading, errMess}){
-    if(isLoading){
+function RenderItem(props) {
+
+    const {item} = props;
+
+    if (props.isLoading) {
         return <Loading />;
     }
-
-    if (errMess) {
+    if (props.errMess) {
         return (
             <View>
-                <Text>{errMess}</Text>
+                <Text>{props.errMess}</Text>
             </View>
         );
     }
-    
-    if(item){
-        return(
+
+    if (item) {
+        return (
             <Card
                 featuredTitle={item.name}
                 image={{uri: baseUrl + item.image}}>
-                
-                <Text style={{margin: 10}} >
+                <Text
+                    style={{margin: 10}}>
                     {item.description}
                 </Text>
             </Card>
         );
     }
-
-    return <View/>
+    return <View />;
 }
-
-
-const mapStateToProps = (state) => {
-    return {
-        campsites   : state.campsites,
-        promotions  : state.promotions,
-        partners    : state.partners
-    };
-}
-
 
 class Home extends Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             scaleValue: new Animated.Value(0)
-        }
+        };
     }
 
-
-
-    animate(){
+    animate() {
         Animated.timing(
             this.state.scaleValue,
             {
                 toValue: 1,
-                duration: 1000
+                duration: 2000
             }
-        ).start()
+        ).start();
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.animate();
     }
 
     static navigationOptions = {
-        title: "Home"
+        title: 'Home'
     }
 
-    render(){
-        const { campsites, promotions, partners } = this.props;
+    render() {
         return (
-            <Animated.ScrollView style={{ transform: [{scale: this.state.scaleValue}] }} >
+            <Animated.ScrollView style={{transform: [{scale: this.state.scaleValue}]}}>
                 <RenderItem 
-                    item={campsites.campsites.filter(camp => camp.featured)[0]} 
+                    item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]} 
                     isLoading={this.props.campsites.isLoading}
                     errMess={this.props.campsites.errMess}
-                />
+                    />
                 <RenderItem 
-                    item={promotions.promotions.filter(promotion => promotion.featured)[0]} 
+                    item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
                     isLoading={this.props.promotions.isLoading}
                     errMess={this.props.promotions.errMess}
-                />
+                    />
                 <RenderItem 
-                    item={partners.partners.filter(partner => partner.featured)[0]} 
+                    item={this.props.partners.partners.filter(partner => partner.featured)[0]}
                     isLoading={this.props.partners.isLoading}
                     errMess={this.props.partners.errMess}
-                />
+                    />
             </Animated.ScrollView>
         );
     }
